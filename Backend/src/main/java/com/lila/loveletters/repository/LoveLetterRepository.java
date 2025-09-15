8417330ab14c16d5c1b330f79e0134f331896357
SharjeelAhmed2 <sharjeelahmed614@gmail.com>
2025-09-16 01:44:05 +0500

    Fixed Repository class and made it into an interface 🙏

--- Backend/src/main/java/com/lila/loveletters/repository/LoveLetterRepository.java
+++ Backend/src/main/java/com/lila/loveletters/repository/LoveLetterRepository.java
@@ -1,4 +1,9 @@
 package com.lila.loveletters.repository;
+import com.lila.loveletters.model.LoveLetter;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class LoveLetterRepository {
+import java.util.List;
+import java.util.UUID;
+public interface LoveLetterRepository extends JpaRepository<LoveLetter, UUID> {
+    List<LoveLetter> findByMood(String mood);
 }
