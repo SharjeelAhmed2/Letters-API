f8968d15a91e9be77db2ddf181313a3dfb40b497
SharjeelAhmed2 <sharjeelahmed614@gmail.com>
2025-09-16 01:40:39 +0500

    Added Model Class

--- Backend/src/main/java/com/lila/loveletters/model/LoveLetter.java
+++ Backend/src/main/java/com/lila/loveletters/model/LoveLetter.java
@@ -1,4 +1,29 @@
 package com.lila.loveletters.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoveLetter {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String author; // optional
+
+    @Column(length = 1000)
+    private String content;
+
+    private String mood; // flirty, comforting, playful, wholesome, etc.
+
+    private LocalDateTime createdAt;
+
 }
